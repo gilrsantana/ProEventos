f04ad1354a9438e3506902d64e03e02d2345e218
gilrsantana <g.r.santana@hotmail.com>
2022-02-26 07:24:49 -0300

    Formulário reativo (evento-detalhe)

--- Front/ProEventos.App/src/app/app.module.ts
+++ Front/ProEventos.App/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -53,6 +53,7 @@ import { RegistrationComponent } from './components/user/registration/registrati
 		BrowserAnimationsModule,
 		HttpClientModule,
 		FormsModule,
+		ReactiveFormsModule,
 		CollapseModule.forRoot(),
 		TooltipModule.forRoot(),
 		BsDropdownModule.forRoot(),
@@ -64,6 +65,7 @@ import { RegistrationComponent } from './components/user/registration/registrati
 			progressBar: true
 		}),
 		NgxSpinnerModule
+
 	],
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 	providers: [EventoService],
--- Front/ProEventos.App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Front/ProEventos.App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-evento-detalhe',
-  templateUrl: './evento-detalhe.component.html',
-  styleUrls: ['./evento-detalhe.component.scss']
+	selector: 'app-evento-detalhe',
+	templateUrl: './evento-detalhe.component.html',
+	styleUrls: ['./evento-detalhe.component.scss']
 })
 export class EventoDetalheComponent implements OnInit {
+	form: FormGroup;
 
-  constructor() { }
+	get f(): any {
+		return this.form.controls;
+	}
 
-  ngOnInit(): void {
-  }
+	constructor(private fb: FormBuilder) {
+		this.form = new FormGroup({});
 
+	}
+
+	ngOnInit(): void {
+
+		this.validation();
+	}
+
+	public validation(): void {
+
+		this.form = this.fb.group({
+			tema: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+			local: ['', Validators.required],
+			dataEvento: ['', [Validators.required]],
+			qtdPessoas: ['', [Validators.required, Validators.min(50), Validators.max(120000)]],
+			telefone: ['', Validators.required],
+			email: ['', [Validators.required, Validators.email]],
+			imagemURL: ['', Validators.required],
+		})
+	}
+
+	public resetForm(): void {
+		this.form.reset();
+	}
 }
